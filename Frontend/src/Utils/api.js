a581e03cb9148c5da5815d58a135c59cb280ad2a
omprakash779 <152632511+omprakash779@users.noreply.github.com>
2026-04-10 16:47:59 +0530

    Update api.js

--- Frontend/src/Utils/api.js
+++ Frontend/src/Utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-   baseURL: 'https://prescripto-p0e4.onrender.com', // 👈 important
+   baseURL: 'https://prescripto-p0e4.onrender.com/api', // 👈 important
 })
 
 // 🔐 Attach token automatically
